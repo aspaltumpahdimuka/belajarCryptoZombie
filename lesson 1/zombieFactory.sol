6c54a26b0ebb548123670ee530ead73a87020688
aspaltumpahdimuka <bountygus@gmail.com>
2023-01-24 11:53:57 +0700

    contract

---
+++ lesson 1/zombieFactory.sol
@@ -0,0 +1,17 @@
+// SPDX-LICENSE-IDENTIFIER: MIT
+/*
+Semua kode sumber Solidity harus dimulai dengan "pragma versi" - deklarasi versi
+ kompiler Solidity yang akan digunakan oleh kode ini. 
+ Hal ini untuk mencegah masalah dengan versi kompiler di masa depan yang berpotensi 
+ memperkenalkan perubahan yang akan merusak kode Anda.
+*/
+pragma solidity ^0.8.0;
+
+/*
+Kode Solidity dikemas dalam kontrak. 
+Kontrak adalah blok bangunan fundamental dari aplikasi Ethereum - semua variabel dan fungsi 
+termasuk dalam kontrak, dan ini akan menjadi titik awal dari semua proyek Anda.
+*/
+contract ZombieFactory {
+    
+}
