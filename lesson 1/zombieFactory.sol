d229ddc5f773e4b24e9d2128d0d44c08ab93c5a3
aspaltumpahdimuka <bountygus@gmail.com>
2023-01-24 12:01:03 +0700

    operasi matematika

--- lesson 1/zombieFactory.sol
+++ lesson 1/zombieFactory.sol
@@ -24,4 +24,18 @@ contract ZombieFactory {
     non-negatif. Ada juga tipe data int untuk bilangan bulat bertanda.
     */
     uint dnaDigits = 16;
+
+    /*
+    Matematika dalam Solidity cukup mudah. Operasi berikut ini sama dengan kebanyakan bahasa 
+    pemrograman:
+
+    Penambahan: x + y
+    Pengurangan: x - y,
+    Perkalian: x * y
+    Pembagian: x / y
+    Modulus / sisa: x % y (misalnya, 13 % 5 adalah 3, karena jika Anda membagi 5 menjadi 13, 
+    3 adalah sisanya)
+    Solidity juga mendukung operator eksponensial (yaitu "x pangkat y", x^y):
+    */
+    uint dnaModulus = 10 ** dnaDigits; // 10^16
 }
